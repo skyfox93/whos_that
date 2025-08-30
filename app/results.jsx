86fbb26ebab52fe627a839ac8619aeb5827752da
Skylar Salerno <skylar.salerno@gmail.com>
2025-08-30 14:25:10 -0400

    updates

--- app/results.jsx
+++ app/results.jsx
@@ -29,7 +29,7 @@ const Results = () => {
   const getImdbLink = async (face) => {
     const imbdUrl = face.Urls.find(url => url.includes("imdb.com"));
     if (imbdUrl) {
-      return imbdUrl
+      return imbdUrl.replace("https://", "").replace("http://", "");
     } 
     if (face.Urls && face.Urls[0]) {
         let id = await fetchImdbId(face.Urls[0].split("/").pop());
@@ -39,7 +39,7 @@ const Results = () => {
     return null;
   };
 
-  const handlePhotoChange = async (event) => {
+  const handlePhotoChange = (event) => {
     const file = event.target.files[0];
     setImage(URL.createObjectURL(file));
     console.log("Selected file:", file);
@@ -50,7 +50,6 @@ const Results = () => {
       const imageBytes = e.target.result.split(",")[1]; // Remove the base64 prefix
       const [response, message] = await recognise(imageBytes);
       setResult(response);
-      console.log(response);
       let updatedResponse = response;
       setLoading(false);
       setChecking(true);
@@ -78,51 +77,60 @@ const Results = () => {
   };
 
   return (
-    <form style={{ padding: "20px" }}>
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <div style={{ position: "relative", display: "inline-block" }}>
       <img src="https://cdn.mos.cms.futurecdn.net/CsvAuuLtsSQj2BBZypvaC7-1200-80.jpg.webp" height="100px" width="auto" style={{ position: "relative" }}/>
-      <svg width="50" height="50" style={{ position: "absolute", top: 30, left: 80 }}>
+      <svg width="50" height="50" style={{ position: "absolute", top: 10, left: 60 }}>
         <rect width="50" height="50" style={{ fill: "rgba(255, 255, 255, 0.2)", stroke: "red", strokeWidth: 2 }} />
       </svg>
-      <h3>Who's that actor?</h3>
+      </div>
+      <div style={{ textAlign: "center" }}>
+      <h2>Who's that actor?</h2>
       {(!result) ? "Identify actors using your smartphone camera. Take or upload a photo to begin" : null}
       <br></br>
+      </div>
       <input type="file" accept="image/*" onChange={handlePhotoChange} />
       {loading ? "loading..." : null}
       {result && (
-        <div>
-          <img src={image} alt="Uploaded" style={{ maxWidth: "300px" }} />
+        <div style={{ marginTop: "20px" ,display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
+          <img src={image} alt="Uploaded" style={{ maxHeight: "200px" }} />
           <h3>In this image:</h3>
           {result.CelebrityFaces && result.CelebrityFaces.length > 0 ? (
-            <ul>
-              {result.CelebrityFaces.map((celebrity, index) => (
-                <li key={index}>
+            <ul style={{textAlign: "center"}}>
+              {result.CelebrityFaces.map((celebrity, index) => {
+               const backgroundSize = (0.70 / celebrity.Face.BoundingBox.Width) * 100;
+                return (
+                <li key={celebrity.Name + index} style={{ margin: "10px auto", listStyleType: "none", textAlign: "left" }}>
                   <div
                     style={{
                       backgroundImage: `url("${image}")`,
-                      backgroundPosition: `${celebrity.Face.BoundingBox.Left * 120}% ${celebrity.Face.BoundingBox.Top * 120}%`,
-                      backgroundSize: `${50 / celebrity.Face.BoundingBox.Width}% ${50 / celebrity.Face.BoundingBox.Height}%`,
-                      border: "1px solid #ccc",
+                      backgroundPosition: `left -${(celebrity.Face.BoundingBox.Left - celebrity.Face.BoundingBox.Width/2) * backgroundSize}px top ${(celebrity.Face.BoundingBox.Top + celebrity.Face.BoundingBox.Height/2) * 100}%`,
+                      backgroundSize: `${backgroundSize}%`,
+                      border: "2px solid #666",
                       display: "inline-block",
-                      marginRight: "10px",
                       width: "100px",
                       height: "100px",
                       verticalAlign: "middle",
+                      marginRight: "10px",
+                      boxShadow: "0 0 5px rgba(0,0,0,0.3)",
+                      borderRadius: "8px",
+                    
                     }}>
                    
                   </div>
 
-                  <a target="_blank" href={`https://${celebrity.imdb}`}>{celebrity.Name}</a> - Confidence:{" "}
-                  {celebrity.MatchConfidence.toFixed(2)}%
+                  <a target="_blank" href={`https://${celebrity.imdb}`}>{celebrity.Name}</a><span style={{ marginLeft: "5px", fontSize: "0.8em", color: "#666" }}>{celebrity.MatchConfidence.toFixed(0)}{"% Match"}</span>
+
                   {checking ? "searching imbd..." : null}
                 </li>
-              ))}
+              )})}
             </ul>
           ) : (
             <p>No celebrities recognized in the photo.</p>
           )}
         </div>
       )}
-    </form>
+    </div>
   )
 }
 export default Results
